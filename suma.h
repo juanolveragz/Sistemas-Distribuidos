9622d030766ea82c9a7fde99d3582fcf7455d1ca
juanolveragz <JuanCarlos@MacBook-Pro-de-Juan.local>
2017-02-08 22:39:32 -0600

    Second commit

--- suma.h
+++ suma.h
@@ -6,9 +6,12 @@
 #ifndef _SUMA_H_RPCGEN
 #define _SUMA_H_RPCGEN
 
-#define RPCGEN_VERSION	199506
-
 #include <rpc/rpc.h>
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 
 struct intpair {
@@ -16,32 +19,35 @@ struct intpair {
 	int b;
 };
 typedef struct intpair intpair;
-#ifdef __cplusplus
-extern "C" bool_t xdr_intpair(XDR *, intpair*);
-#elif __STDC__
-extern  bool_t xdr_intpair(XDR *, intpair*);
-#else /* Old Style C */
-bool_t xdr_intpair();
-#endif /* Old Style C */
 
+#define SUMA_PROG 0x23451111
+#define SUMA_VERS 1
 
-#define SUMA_PROG ((rpc_uint)0x23451111)
-#define SUMA_VERS ((rpc_uint)1)
-
-#ifdef __cplusplus
-#define SUMA ((rpc_uint)1)
-extern "C" int * suma_1(intpair *, CLIENT *);
-extern "C" int * suma_1_svc(intpair *, struct svc_req *);
-
-#elif __STDC__
-#define SUMA ((rpc_uint)1)
+#if defined(__STDC__) || defined(__cplusplus)
+#define SUMA 1
 extern  int * suma_1(intpair *, CLIENT *);
 extern  int * suma_1_svc(intpair *, struct svc_req *);
+extern int suma_prog_1_freeresult (SVCXPRT *, xdrproc_t, caddr_t);
 
-#else /* Old Style C */
-#define SUMA ((rpc_uint)1)
+#else /* K&R C */
+#define SUMA 1
 extern  int * suma_1();
 extern  int * suma_1_svc();
-#endif /* Old Style C */
+extern int suma_prog_1_freeresult ();
+#endif /* K&R C */
+
+/* the xdr functions */
+
+#if defined(__STDC__) || defined(__cplusplus)
+extern  bool_t xdr_intpair (XDR *, intpair*);
+
+#else /* K&R C */
+extern bool_t xdr_intpair ();
+
+#endif /* K&R C */
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* !_SUMA_H_RPCGEN */
